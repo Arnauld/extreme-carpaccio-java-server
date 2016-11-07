ff62ba668d8b29c65ec9aed4d2e7e986bb767972
Arnauld <arnauld.loyer@gmail.com>
2016-11-08 00:44:11 +0100

    reformat

--- src/main/java/fr/arolla/XcServerApplication.java
+++ src/main/java/fr/arolla/XcServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class XcServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(XcServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(XcServerApplication.class, args);
+    }
 }

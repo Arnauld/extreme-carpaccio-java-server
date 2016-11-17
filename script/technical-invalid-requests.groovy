673d632f6389c9a9dfe95e85f61fd90f04538454
fmaury <fab.maury@gmail.com>
2016-11-17 15:08:48 +0100

    fixup! [Feature] invalid technical request

--- script/technical-invalid-requests.groovy
+++ script/technical-invalid-requests.groovy
@@ -8,28 +8,13 @@
 import fr.arolla.core.Question
 import fr.arolla.core.QuestionGenerator
 import fr.arolla.core.question.QuestionMultipleChoice
+import fr.arolla.core.question.RandomBytesQuestion
 import fr.arolla.util.Randomizator
-
-import java.util.function.Predicate
-import java.util.stream.Collectors
-
 // ----------------------------------------------------------------------------
 //
 // HELPER FUNCTIONS
 //
 // ----------------------------------------------------------------------------
-
-def q(String question, Predicate<String> validator) {
-    return new QuestionMultipleChoice(question, validator)
-}
-
-def q(String question, List<String> choices, String response) {
-    return new QuestionMultipleChoice(question, choices, response)
-}
-
-def q(String question, List<String> choices, Predicate<String> validator) {
-    return new QuestionMultipleChoice(question, choices, validator)
-}
 
 // ----------------------------------------------------------------------------
 //
@@ -39,7 +24,7 @@ def q(String question, List<String> choices, Predicate<String> validator) {
 
 // hint field to check in log if your configuration have been reloaded
 
-version = "1.0.1"
+version = "1.0.0"
 
 // ----------------------------------------------------------------------------
 //
@@ -47,24 +32,18 @@ version = "1.0.1"
 //
 // ----------------------------------------------------------------------------
 
-weight = 0.9 as double
+weight = 0.0 as double
 
 // ----------------------------------------------------------------------------
 //
 // QUESTIONS
 //
 // ----------------------------------------------------------------------------
+Randomizator random = new Randomizator();
 
 def questions = [
-        q("What's the name of your team?", { s -> true }),
-        q("Which word in the list designate a programming language?", ["jawa", "jaba", "java"], "java"),
-        q("Is this expression valid: '2+4i'?", ['yes', 'no'], "yes"),
-        q("A monoid is an algebraic structure having? (select all that applies, separated by a comma)",
-                ["an identity element", "a binary associative operation", "a milkshake", "the sinus operator", "a ternary bijective operator"],
-                { s ->
-                    def xs = (s.split(",") as List).stream().map({ x -> x.trim() }).collect(Collectors.toList())
-                    xs.containsAll("an identity element", "a binary associative operation") && xs.size() == 2
-                })
+        new RandomBytesQuestion(5_000),
+        new RandomBytesQuestion(1_000_000),
 ] as List
 
 
